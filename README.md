d268ba41886fc0398cf98dbafe3f7789d6d61c87
Marcelino Llano <hello@marcelinollano.com>
2014-09-14 21:49:39 +0200

    Start cleaning up the script

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-README
-======
+Twitter Purge
+=============
--- twitter-purge.rb
+++ twitter-purge.rb
@@ -4,27 +4,30 @@ require 'date'
 require 'rubygems'
 require 'twitter'
 
-USERNAME = '<username>'
-ATTEMPTS = 1
-DAYS     = 5
+USERNAME            = '<username>'
+CONSUMER_KEY        = '<consumer-key>'
+CONSUMER_SECRET     = '<consumer-secret>'
+ACCESS_TOKEN        = '<access-token>'
+ACCESS_TOKEN_SECRET = '<access-token-secret>'
+DAYS_TO_KEEP        = 5
+ATTEMPTS_LIMIT      = 1
 
-config = {
-  :consumer_key        => "<consumer-key>",
-  :consumer_secret     => "<consumer-secret>",
-  :access_token        => "<access-token>",
-  :access_token_secret => "<access-token-secret>"
-}
+client = Twitter::REST::Client.new({
+  :consumer_key        => CONSUMER_KEY,
+  :consumer_secret     => CONSUMER_SECRET,
+  :access_token        => ACCESS_TOKEN,
+  :access_token_secret => ACCESS_TOKEN_SECRET
+})
 
-client = Twitter::REST::Client.new(config)
-num_attempts = 0
+attempts = 0
 begin
-  num_attempts += 2
-  date_limit = (Date.today - DAYS).to_time
+  attempts += 2
+  date_limit = (Date.today - DAYS_TO_KEEP).to_time
   client.user_timeline(USERNAME, :count => '150').each do |t|
     client.destroy_status(t.id) if t.created_at < date_limit;
   end
 rescue Twitter::Error::TooManyRequests => error
-  if num_attempts <= ATTEMPTS
+  if attempts <= ATTEMPTS_LIMIT
     sleep error.rate_limit.reset_in
     retry
   else
